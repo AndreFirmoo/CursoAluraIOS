2b9bc76432b8f1ca2746b49ff1303991a7c8bc0a
AndreFirmoo <nsd_afirmo@uolinc.com>
2020-12-25 23:31:34 -0300

    Criando outlets para tableviewCell

---
+++ Alura VIagens/Alura VIagens/ViagemTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ViagemTableViewCell.swift
+//  Alura VIagens
+//
+//  Created by Andre Firmo on 25/12/20.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var TituloLB: UILabel!
+    @IBOutlet weak var QuantidadeDiasLB: UILabel!
+    @IBOutlet weak var precoLB: UILabel!
+    @IBOutlet weak var ImagemViagem: UIImageView!
+}
