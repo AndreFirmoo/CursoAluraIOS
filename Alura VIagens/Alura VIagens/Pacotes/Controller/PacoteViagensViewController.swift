70673c8d3af91f3528f41ef642cdb7c4cd3314ab
AndreFirmoo <nsd_afirmo@uolinc.com>
2020-12-26 15:32:05 -0300

    Incluindo constransts na UICollectionViewCell e adequando a largura da cell

--- Alura VIagens/Alura VIagens/Pacotes/Controller/PacoteViagensViewController.swift
+++ Alura VIagens/Alura VIagens/Pacotes/Controller/PacoteViagensViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class PacoteViagensViewController: UIViewController {
 
+    let listaViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
+    
     @IBOutlet weak var colecaoDePacotes: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,16 +21,36 @@ class PacoteViagensViewController: UIViewController {
 }
 extension PacoteViagensViewController : UICollectionViewDelegate{
     
+
 }
+
+extension PacoteViagensViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let largura = collectionView.bounds.width / 2
+        return CGSize(width: largura-15, height: 160)
+    }
+}
+
 extension PacoteViagensViewController : UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return listaViagens.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! CelulaViagemCollectionViewCell
+        
+        let dados = listaViagens[indexPath.item]
+        
+        cell.CidadeImagem.image = UIImage(named: dados.caminhoImagem)
+        cell.CidadeLb.text = dados.titulo
+        cell.PrecoLB.text = dados.preco
+        cell.QuantidadeDeDiasLB.text = dados.quantidadeDeDias
+        
+        cell.layer.borderWidth = 0.5
+        cell.layer.borderColor = CGColor(red: 85.0/255, green: 85.0/255, blue: 85.0/255, alpha: 1)
+        cell.layer.cornerRadius = 10
+        
         return cell
     }
-    
-    
 }
+
