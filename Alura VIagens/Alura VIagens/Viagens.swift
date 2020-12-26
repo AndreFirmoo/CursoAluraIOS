db227c2d5ffbd600c409d6a276c95cc5005f85fa
AndreFirmoo <nsd_afirmo@uolinc.com>
2020-12-25 23:07:30 -0300

    Criando a classe viagem, necessaria para referenciar a tableView

---
+++ Alura VIagens/Alura VIagens/Viagens.swift
@@ -0,0 +1,23 @@
+//
+//  Viagens.swift
+//  Alura VIagens
+//
+//  Created by Andre Firmo on 25/12/20.
+//
+
+import UIKit
+
+class Viagem: NSObject {
+    let titulo: String
+    let quantidadeDeDias: String
+    let preco: String
+    let caminhoImagem: String
+    
+    init(titulo: String, quantidadeDeDias: String, preco: String, caminhoImagem: String) {
+        self.titulo = titulo
+        self.quantidadeDeDias = quantidadeDeDias
+        self.preco = preco
+        self.caminhoImagem = caminhoImagem
+    }
+    
+}
